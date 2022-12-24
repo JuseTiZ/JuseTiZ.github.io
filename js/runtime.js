ffeede462c3ff42a4bb993359ab74b0a619cf023
JuseTiZ <291195857@qq.com>
2022-12-24 23:20:46 +0800

    Site updated: 2022-12-24 23:20:45

---
+++ js/runtime.js
@@ -0,0 +1,36 @@
+setInterval(() => {
+  let create_time = Math.round(new Date('2022-10-30 00:22:00').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<22) && (Number(time[2])>7)){
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/%E2%99%82-Juse%20%E6%89%93%E5%B7%A5%E4%B8%AD-blue' title='为了科研加油吧'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div><br>这个博客竟然已经运行了那么久了<br>以后也请多关照';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/%F0%9F%92%A4-Juse%20%E7%9D%A1%E7%9C%A0%E4%B8%AD-blue' title='摸鱼时间到了给自己放个假'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>这个博客竟然已经运行了那么久了<br>以后也请多关照';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
