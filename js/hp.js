1ebd06dba84918e67586bddf658c6cd6b8736dbe
JuseTiZ <291195857@qq.com>
2023-11-22 21:12:08 +0800

    Site updated: 2023-11-22 21:12:00

--- js/hp.js
+++ js/hp.js
@@ -2,8 +2,7 @@ const textContent = document.getElementById("textContent");
 const textModule = document.getElementById("textModule");
 const texts = [
                 '最大的希望：家人健康幸福',
-                '目前的计划：科研有所成就',
-                '当前的状态：绝赞课题组办公中',
+                '目前的计划：搞好自己的课题',
                 '现有的成就：谈了个可爱女朋友',
               ];
 let currentIndex = 0;
